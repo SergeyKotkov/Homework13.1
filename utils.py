73d315364b3d9d478d593803b0bac16680222320
XE <Smoke69rus@rambler.ru>
2024-03-25 22:42:37 +0300

    add file

--- utils.py
+++ utils.py
@@ -2,10 +2,32 @@ class Category:
     name: str
     description: str
     product: list
+    total_categories = 0
+    total_uniq_products = set()
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.total_categories += 1
+        for product in products:
+            Category.total_uniq_products.add(product.name)
 
 
 
 
-class Product(Category):
+
+class Product():
+    name: str
+    description: str
+    product: list
     price: float
     stock: int
+
+    def __init__(self, name, description, product, price, stock):
+        self.price = price
+        self.stock = stock
+        self.name = name
+        self.description = description
+        self.product = product
+
