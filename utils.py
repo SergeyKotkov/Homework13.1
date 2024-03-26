bb09e26205e2bb1e7947b00bfd797cfabdf67f1a
XE <Smoke69rus@rambler.ru>
2024-03-26 21:46:17 +0300

    add test

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ class Category:
         self.products = products
         Category.total_categories += 1
         for product in products:
-            Category.total_uniq_products.add(product.name)
+            Category.total_uniq_products.add(product)
 
 
 
@@ -31,3 +31,23 @@ class Product():
         self.description = description
         self.product = product
 
+if __name__ == '__main__':
+
+    prod_1 = Category('Яблоки', 'сладкие', ("фрукт", "красные"))
+    prod_2 = Category('Бананы', 'спелые', ('фрукт', 'желтые'))
+    prod_3 = Category('Яблоки', 'кислые', ('фрукт', 'зеленые'))
+    prod_4 = Category('Конфеты', 'вкусные', ('сладости', 'шоколадные'))
+    prod_5 = Category('Картофель', 'крупная', ('овощи', 'мытый'))
+
+    Prod_1 = Product('Яблоки', 'сладкие', ("фрукт", "красные"),50, 100)
+    Prod_2 = Product('Бананы', 'спелые', ("фрукт", "желтые"),100, 200)
+    Prod_3 = Product('Конфеты', 'сладкие', ("сладости", "шоколадные"),75, 1000)
+    Prod_4 = Product('Картофель', 'крупный', ("овощи", "мытый"),65, 2000)
+
+    print(f'{prod_1.total_categories}')
+    print(f'{prod_2.products}')
+    print(f'{prod_3.name}')
+    print(f'{prod_4.description}')
+    print(f'{Prod_1.price}', f'{Prod_1.stock}')
+    print(f'{Prod_2.description}')
+    print(f'{Prod_3.product}')
