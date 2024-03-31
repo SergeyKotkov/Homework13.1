d70fa7a9489f8ea0080b34679047cb9e38e982a4
XE <Smoke69rus@rambler.ru>
2024-03-31 19:17:22 +0300

    add poetry and classmethod

--- test/tests.py
+++ test/tests.py
@@ -5,27 +5,26 @@ from utils import Category, Product
 
 @pytest.fixture()
 def Category_prod_1():
-    return Category('Яблоки', 'сладкие', ("фрукт", "красные"))
+    return Category('Яблоки', 'сладкие')
 
 
 def test_Category(Category_prod_1):
     assert Category_prod_1.name == 'Яблоки'
     assert Category_prod_1.description == 'сладкие'
-    assert Category_prod_1.products == ("фрукт", "красные")
+
 
 @pytest.fixture()
 def Product_prod_1():
-    return Product('Яблоки', 'сладкие', ("фрукт", "красные"),50, 100)
+    return Product('Яблоки', 'сладкие',  50, 100)
 
 def test_Product(Product_prod_1):
     assert Product_prod_1.name == 'Яблоки'
     assert Product_prod_1.description == 'сладкие'
-    assert Product_prod_1.product == ("фрукт", "красные")
     assert Product_prod_1.price == 50
-    assert Product_prod_1.stock == 100
+    assert Product_prod_1.quantity == 100
 
 def test_total_categories(Category_prod_1):
     assert Category.total_categories > 0
 
-def test_total_unique_products(Product_prod_1):
-    assert len(Category.total_uniq_products) > 0
+def test_total_uniq_products(Product_prod_1):
+    assert Category.total_uniq_products != None
